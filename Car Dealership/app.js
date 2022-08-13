df3e777c275ec0d68c6b3534c4486973e288809a
GM-24 <60579146+GM-24@users.noreply.github.com>
2022-08-13 17:32:34 +0400

    Add files via upload

---
+++ Car Dealership/app.js
@@ -0,0 +1,260 @@
+const cars = [
+  {
+    id: 1,
+    model: "BMW 3.0 CSL",
+    manufacturer: "bmw",
+
+    price: 99.999,
+    img: "./resized/bmw1.jpg",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, nobis
+            molestias voluptatem, explicabo, necessitatibus dolores nihil
+            consequuntur aliquid ut amet distinctio fuga nisi officiis sed quia
+            minus sapiente enim dicta!`,
+  },
+  {
+    id: 2,
+    model: "BMW E31",
+    manufacturer: "bmw",
+    price: 99.999,
+    img: "./resized/bmw2.jpg",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, nobis
+            molestias voluptatem, explicabo, necessitatibus dolores nihil
+            consequuntur aliquid ut amet distinctio fuga nisi officiis sed quia
+            minus sapiente enim dicta!`,
+  },
+
+  {
+    id: 3,
+    model: "BMW E30 M3",
+    manufacturer: "bmw",
+    price: 99.999,
+    img: "./resized/bmw3.jpg",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, nobis
+            molestias voluptatem, explicabo, necessitatibus dolores nihil
+            consequuntur aliquid ut amet distinctio fuga nisi officiis sed quia
+            minus sapiente enim dicta!`,
+  },
+
+  {
+    id: 4,
+    model: "BMW i8",
+    manufacturer: "bmw",
+    price: 99.999,
+    img: "./resized/bmw4.jpg",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, nobis
+            molestias voluptatem, explicabo, necessitatibus dolores nihil
+            consequuntur aliquid ut amet distinctio fuga nisi officiis sed quia
+            minus sapiente enim dicta!`,
+  },
+
+  {
+    id: 5,
+    model: "BMW E30 M3",
+    manufacturer: "bmw",
+    price: 99.999,
+    img: "./resized/bmw5.jpg",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, nobis
+            molestias voluptatem, explicabo, necessitatibus dolores nihil
+            consequuntur aliquid ut amet distinctio fuga nisi officiis sed quia
+            minus sapiente enim dicta!`,
+  },
+  {
+    id: 6,
+    model: "Bugatti Chiron",
+    manufacturer: "bugatti",
+    price: 99.999,
+    img: "./resized/bug1.jpg",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, nobis
+            molestias voluptatem, explicabo, necessitatibus dolores nihil
+            consequuntur aliquid ut amet distinctio fuga nisi officiis sed quia
+            minus sapiente enim dicta!`,
+  },
+
+  {
+    id: 7,
+    model: "Bugatti Veyron",
+    manufacturer: "bugatti",
+    price: 99.999,
+    img: "./resized/bug2.jpg",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, nobis
+            molestias voluptatem, explicabo, necessitatibus dolores nihil
+            consequuntur aliquid ut amet distinctio fuga nisi officiis sed quia
+            minus sapiente enim dicta!`,
+  },
+
+  {
+    id: 8,
+    model: "Koenigsegg Agera R",
+    manufacturer: "koenigsegg",
+    price: 99.999,
+    img: "./resized/koen.jpg",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, nobis
+            molestias voluptatem, explicabo, necessitatibus dolores nihil
+            consequuntur aliquid ut amet distinctio fuga nisi officiis sed quia
+            minus sapiente enim dicta!`,
+  },
+  {
+    id: 9,
+    model: "McLaren 720s",
+    manufacturer: "mclaren",
+    price: 99.999,
+    img: "./resized/mc1.jpg",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, nobis
+            molestias voluptatem, explicabo, necessitatibus dolores nihil
+            consequuntur aliquid ut amet distinctio fuga nisi officiis sed quia
+            minus sapiente enim dicta!`,
+  },
+  {
+    id: 10,
+    model: "McLaren Senna",
+    manufacturer: "mclaren",
+    price: 99.999,
+    img: "./resized/mc2.jpg",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, nobis
+            molestias voluptatem, explicabo, necessitatibus dolores nihil
+            consequuntur aliquid ut amet distinctio fuga nisi officiis sed quia
+            minus sapiente enim dicta!`,
+  },
+
+  {
+    id: 11,
+    model: "Mercedes-Benz 300SL",
+    manufacturer: "mercedes",
+    price: 99.999,
+    img: "./resized/merc1.jpg",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, nobis
+            molestias voluptatem, explicabo, necessitatibus dolores nihil
+            consequuntur aliquid ut amet distinctio fuga nisi officiis sed quia
+            minus sapiente enim dicta!`,
+  },
+
+  {
+    id: 12,
+    model: "Mercedes-Benz G-Wagon Brabus",
+    manufacturer: "mercedes",
+    price: 99.999,
+    img: "./resized/merc2.jpg",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, nobis
+            molestias voluptatem, explicabo, necessitatibus dolores nihil
+            consequuntur aliquid ut amet distinctio fuga nisi officiis sed quia
+            minus sapiente enim dicta!`,
+  },
+
+  {
+    id: 13,
+    model: "Porsche Carrera GT",
+    manufacturer: "porsche",
+    price: 99.999,
+    img: "./resized/por1.jpg",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, nobis
+            molestias voluptatem, explicabo, necessitatibus dolores nihil
+            consequuntur aliquid ut amet distinctio fuga nisi officiis sed quia
+            minus sapiente enim dicta!`,
+  },
+
+  {
+    id: 14,
+    model: "Porsche GT-2 RS",
+    manufacturer: "porsche",
+    price: 99.999,
+    img: "./resized/por2.jpg",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, nobis
+            molestias voluptatem, explicabo, necessitatibus dolores nihil
+            consequuntur aliquid ut amet distinctio fuga nisi officiis sed quia
+            minus sapiente enim dicta!`,
+  },
+  {
+    id: 15,
+    model: "Porsche GT-3 RS",
+    manufacturer: "porsche",
+    price: 99.999,
+    img: "./resized/por3.jpg",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, nobis
+            molestias voluptatem, explicabo, necessitatibus dolores nihil
+            consequuntur aliquid ut amet distinctio fuga nisi officiis sed quia
+            minus sapiente enim dicta!`,
+  },
+  {
+    id: 16,
+    model: "Elefant(AKA Ferdiand)",
+    manufacturer: "porsche",
+    price: 99.999,
+    img: "./resized/por4.jpg",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, nobis
+            molestias voluptatem, explicabo, necessitatibus dolores nihil
+            consequuntur aliquid ut amet distinctio fuga nisi officiis sed quia
+            minus sapiente enim dicta!`,
+  },
+];
+
+const sectionCenter = document.querySelector(".section-center");
+
+window.addEventListener("DOMContentLoaded", function () {
+  displayCars(cars);
+});
+
+function displayCars(Cars) {
+  let showCars = Cars.map(function (car) {
+    return `<article class="single-car">
+        <img src=${car.img} alt="" class="img"/>
+        <div class="car-info">
+          <h4 class="car-name">${car.model}</h4>
+          <p class="time">   </p>
+          <h5 class="car-price">price: ${car.price}$</h5>
+          <p class="car-desc">
+            ${car.desc}
+          </p>
+        </div>
+      </article>`;
+  });
+  showCars = showCars.join("");
+  sectionCenter.innerHTML = showCars;
+}
+
+const filterBtns = document.querySelectorAll(".filter-btn");
+
+filterBtns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const manufacturer = e.currentTarget.dataset.id;
+    const carManufacturer = cars.filter(function (car) {
+      if (car.manufacturer === manufacturer) {
+        return car;
+      }
+    });
+    if (manufacturer === "all") {
+      return displayCars(cars);
+    } else {
+      displayCars(carManufacturer);
+    }
+  });
+});
+
+//set the date we're counting down to
+
+let countdownDate = new Date("Sep 1, 2022 00:00:00").getTime();
+
+//this updates time every one second
+let timeFunction = setInterval(function () {
+  //get today's date
+  let now = new Date().getTime();
+
+  //calculate time till the countdown
+  let remainingTime = countdownDate - now;
+
+  //calculationd for days, hours, minutes, seconds
+  let days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(
+    (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+  //display result on html page
+  document.querySelector(".time").innerHTML =
+    days + " D " + hours + " H " + minutes + " M " + seconds + " s ";
+
+  if (remainingTime < 0) {
+    clearInterval(timeFunction);
+    document.querySelector(".time").innerHTML = "Expired";
+  }
+}, 1000);
